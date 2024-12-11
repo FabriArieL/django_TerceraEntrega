84579a0bd844e7dd435b4b21894f4cea16d69056
Fabrizio <fabrizioariel1999@gmail.com>
2024-12-11 16:56:38 -0300

    Segundo guardado

--- django_entrega/urls.py
+++ django_entrega/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import bienvenida, mi_template
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("bienvenida/", bienvenida),
+    path("mi-template/", mi_template)
 ]
